b6ecb1bf74e01ff26b46ba558a1f7f59f3f6df75
ejcris <elysiajelenavilladarez@yahoo.com>
2016-03-21 22:16:43 +0800

    Made scv file and classified non-objects and objects

--- canny.py
+++ canny.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import csv
 
 def auto_canny(image, sigma=0.33):
   # compute the median of the single channel pixel intensities
@@ -23,23 +24,23 @@ def sobelMap(img):
     return sobelMat
 
 def buildNgMat(img):
-    resizedImg = cv2.resize(m, Size(8,8))
+    resizedImg = cv2.resize(img, (8,8))
     return resizedImg
 
 def getNGFeatureVector(img):
     # img must be grayscale
     features = []
-    for i in img.rows:
-        for j in img.columns:
-            features.append(img[i,j])
+    for i in range(0, 8):
+        for j in range(0,8):
+            features.append(img[i][j])
     return features
 
 
 cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
 
-im = cv2.imread('fig1.png',0) # convert to grayscale on read, where contours will be derived from
-g = cv2.imread('fig1.png',3) # where the rects are to be drawn
-to_be_cropped = cv2.imread('fig1.png',3) # image for cropping
+im = cv2.imread('fig2.png',0) # convert to grayscale on read, where contours will be derived from
+g = cv2.imread('fig2.png',3) # where the rects are to be drawn
+to_be_cropped = cv2.imread('fig2.png',0) # image for cropping
 
 edge = auto_canny(im, sigma=20.0)
 img, contours, hierarchy = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -49,11 +50,18 @@ for a in range(0, size):
     c = contours[a]
     x,y,w,h = cv2.boundingRect(c)
     cv2.rectangle(g, (x,y), (x+w,y+h), (0,0,255))
-    if(h > 20 and w > 20):
+    if(h > 10 and w > 10):
         cropped_g = to_be_cropped[y:y+h, x:x+w]
-        cv2.imwrite('images'+str(a)+".jpg", cropped_g)
+        newimg = buildNgMat(sobelMap(buildNgMat(cropped_g)));
+        features = [];
+        features = getNGFeatureVector(newimg)
+        with open('csvfile.csv', 'a') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+            #data = [str(a), features]            
+            spamwriter.writerow(features)
+        #cv2.imwrite('images'+str(a)+".jpg", newimg)
         # cropped_num = cropped_num+1
-    cv2.imshow('image', g)
+cv2.imshow('image', g)
 
 # exit mode
 ch = cv2.waitKey(0)
